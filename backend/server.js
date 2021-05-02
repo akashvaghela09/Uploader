eebf9d555c467bed648856fe3838c3c75ceaeb38
akashvaghela09 <akashvaghela09@gmail.com>
2021-05-02 11:04:45 +0530

    basic uploader added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
 const express = require("express")
+const multer = require("multer")
 const app = express()
 
-app.listen(8000, () => {
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './files')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname)
+    }
+})
+
+const upload = multer({ storage: fileStorageEngine });
+
+app.post("/", upload.single("file"), (req, res) => {
+    console.log(req.file);
+    res.send("Single file uploaded success")
+})
+
+app.listen(process.env.PORT || 8000, () => {
     console.log("Server started");
 })
